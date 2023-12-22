4b4850005d2791812ef052de17cf3f740b734e5d
joy <juniorbat0003@gmail.com>
2023-12-22 23:51:18 +0600

    Design added

--- src/Components/DashBoard/SeeAlluser.js
+++ src/Components/DashBoard/SeeAlluser.js
@@ -14,11 +14,18 @@ const SeeAlluser = () => {
   return (
     <div>
       {allUsers.map((item) => (
-        <p>
-          <b>Name:</b> <i>{item.name}</i> <b>Status:</b>
-          <b>Email{item.email}</b>
-          <img src={item?.nid?.imageUrl} alt="" srcset="" />
-        </p>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src={item?.nid?.imageUrl} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{item?.name}</h2>
+            <h2 className="card-title">{item?.email}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Approve Verification</button>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
